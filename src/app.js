e55d111b30fe748393a0e227c0b554db5f34fd61
Alessandro Parrilla <killerparro@AirdiAlessandro.station>
2022-02-12 21:47:51 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,3 @@
+let func = () => console.log('caro');
+
+func();
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry : {
+        app:  './src/app.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [{
+            //a regex that looks at all Javascrpit files
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }]
+    },
+    devServer: {
+        historyApiFallback: true
+    }
+}
